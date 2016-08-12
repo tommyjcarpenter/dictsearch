0e90ff5ed185b4020e212024f65637b7739afc4b
Tommy Carpenter <tommy@research.att.com>
2016-08-11 21:20:54 -0400

    Added a new function to get all values, switched to pytest

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
       name = "dictsearch",
-      version = "3.0.0",
+      version = "3.1.0",
       author = "Tommy Carpenter",
       author_email = "tommyjcarpenter@gmail.com",
       url = "https://github.com/tommyjcarpenter/dictsearch",
       description = "A tool for iterating dictionaries as trees and printing all leaf nodes at some path",
       license = "MIT License",
       packages=find_packages(),
+      install_requires = ['pytest']
      )
